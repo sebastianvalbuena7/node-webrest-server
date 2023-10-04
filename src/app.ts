0a1bdcea9f794e142056bbcb97f3d4be3348d4ec
Sebastian Valbuena <sebastianvalbuena25@gmail.com>
2023-10-03 19:33:34 -0500

    Variables de entorno con dotenv

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import { envs } from "./config/envs"
+import { Server } from "./presentation/server"
+
+(async () => {
+    main()
+})()
+
+async function main() {
+    const server = new Server({
+        port: envs.PORT,
+        publicPath: envs.PUBLIC_PATH
+    })
+    
+    server.start()
+}
